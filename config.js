52a4b31e03b58d3cfc72758cd3ba73b8ed6298c6
Oshada123456 <Oaluthgedara@gmail.com>
2025-11-05 06:50:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "FcUT1RBR#0bWBZ3yZiqa_HUSED5zEtDheaCvppS7gDueSq3z-bYw",
-  MONGODB: process.env.MONGODB || "mongodb://mongo:ghJxriODeIiINStnjRkEaYCvSYMXPksX@trolley.proxy.rlwy.net:34602",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:aTYHnZOGMGtbjbWMYfliTKUMzhkNiVVZ@switchback.proxy.rlwy.net:16068",
   OWNER_NUM: process.env.OWNER_NUM || "94788345811",
 };
